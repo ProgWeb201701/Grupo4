7feaa1cd8d6dde4f34f5878d37dfd58b8e598095
DESKTOP-0E6E8CP\Rodrigo <rodrigo.blizzard@hotmail.com>
2017-06-22 19:15:59 -0300

    banco de dados - revisado

--- src/BD/BANCO DE DADOS - SISTEMA DE TCC.sql
+++ src/BD/BANCO DE DADOS - SISTEMA DE TCC.sql
@@ -8,19 +8,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema tcc
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema tcc
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `tcc` DEFAULT CHARACTER SET utf8 ;
+USE `tcc` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Professor`
+-- Table `tcc`.`Professor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Professor` (
+CREATE TABLE IF NOT EXISTS `tcc`.`Professor` (
   `siape` VARCHAR(20) NOT NULL,
   `nome` VARCHAR(50) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
@@ -29,9 +29,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Aluno`
+-- Table `tcc`.`Aluno`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Aluno` (
+CREATE TABLE IF NOT EXISTS `tcc`.`Aluno` (
   `matricula` VARCHAR(50) NOT NULL,
   `nome` VARCHAR(50) NOT NULL,
   `senha` VARCHAR(45) NOT NULL,
@@ -40,9 +40,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Monografia`
+-- Table `tcc`.`Monografia`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Monografia` (
+CREATE TABLE IF NOT EXISTS `tcc`.`Monografia` (
   `idMonografia` INT NOT NULL AUTO_INCREMENT,
   `versao` VARCHAR(45) NULL,
   `titulo` VARCHAR(100) NOT NULL,
@@ -57,55 +57,54 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Monografia` (
   INDEX `fk_Monografia_Professor2_idx` (`Professor_Coorientador` ASC),
   CONSTRAINT `fk_Monografia_Aluno1`
     FOREIGN KEY (`aluno_matricula`)
-    REFERENCES `mydb`.`Aluno` (`matricula`)
+    REFERENCES `tcc`.`Aluno` (`matricula`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Monografia_Professor1`
     FOREIGN KEY (`professor_orientador`)
-    REFERENCES `mydb`.`Professor` (`siape`)
+    REFERENCES `tcc`.`Professor` (`siape`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Monografia_Professor2`
     FOREIGN KEY (`Professor_Coorientador`)
-    REFERENCES `mydb`.`Professor` (`siape`)
+    REFERENCES `tcc`.`Professor` (`siape`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Avaliacao`
+-- Table `tcc`.`Avaliacao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Avaliacao` (
+CREATE TABLE IF NOT EXISTS `tcc`.`Avaliacao` (
   `idAvaliacao` INT NOT NULL AUTO_INCREMENT,
   `nota` FLOAT NULL,
   `caminhoFeedback` VARCHAR(100) NULL,
   `observacao` VARCHAR(100) NULL,
-  `` VARCHAR(45) NULL,
   PRIMARY KEY (`idAvaliacao`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Turma`
+-- Table `tcc`.`Turma`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Turma` (
+CREATE TABLE IF NOT EXISTS `tcc`.`Turma` (
   `nomeTurma` VARCHAR(15) NOT NULL,
   `Professor_siape` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`nomeTurma`),
   INDEX `fk_Turma_Professor1_idx` (`Professor_siape` ASC),
   CONSTRAINT `fk_Turma_Professor1`
     FOREIGN KEY (`Professor_siape`)
-    REFERENCES `mydb`.`Professor` (`siape`)
+    REFERENCES `tcc`.`Professor` (`siape`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Prof_Avalia_Monografia`
+-- Table `tcc`.`Prof_Avalia_Monografia`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Prof_Avalia_Monografia` (
+CREATE TABLE IF NOT EXISTS `tcc`.`Prof_Avalia_Monografia` (
   `Professor_siape` VARCHAR(20) NOT NULL,
   `Monografia_idMonografia` INT NOT NULL,
   `Avaliacao_idAvaliacao` INT NOT NULL,
@@ -115,26 +114,26 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Prof_Avalia_Monografia` (
   INDEX `fk_Prof_Avalia_Monografia_Avaliacao1_idx` (`Avaliacao_idAvaliacao` ASC),
   CONSTRAINT `fk_Professor_has_Monografia_Professor1`
     FOREIGN KEY (`Professor_siape`)
-    REFERENCES `mydb`.`Professor` (`siape`)
+    REFERENCES `tcc`.`Professor` (`siape`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Professor_has_Monografia_Monografia1`
     FOREIGN KEY (`Monografia_idMonografia`)
-    REFERENCES `mydb`.`Monografia` (`idMonografia`)
+    REFERENCES `tcc`.`Monografia` (`idMonografia`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Prof_Avalia_Monografia_Avaliacao1`
     FOREIGN KEY (`Avaliacao_idAvaliacao`)
-    REFERENCES `mydb`.`Avaliacao` (`idAvaliacao`)
+    REFERENCES `tcc`.`Avaliacao` (`idAvaliacao`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Turma_has_Aluno`
+-- Table `tcc`.`Turma_has_Aluno`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Turma_has_Aluno` (
+CREATE TABLE IF NOT EXISTS `tcc`.`Turma_has_Aluno` (
   `Turma_nomeTurma` VARCHAR(15) NOT NULL,
   `Aluno_matricula` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`Turma_nomeTurma`, `Aluno_matricula`),
@@ -142,12 +141,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Turma_has_Aluno` (
   INDEX `fk_Turma_has_Aluno_Turma1_idx` (`Turma_nomeTurma` ASC),
   CONSTRAINT `fk_Turma_has_Aluno_Turma1`
     FOREIGN KEY (`Turma_nomeTurma`)
-    REFERENCES `mydb`.`Turma` (`nomeTurma`)
+    REFERENCES `tcc`.`Turma` (`nomeTurma`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Turma_has_Aluno_Aluno1`
     FOREIGN KEY (`Aluno_matricula`)
-    REFERENCES `mydb`.`Aluno` (`matricula`)
+    REFERENCES `tcc`.`Aluno` (`matricula`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
