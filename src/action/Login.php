b6153df9a2d43dea9172d554f711bfe8dfc2f2b9
DESKTOP-0E6E8CP\Rodrigo <rodrigo.blizzard@hotmail.com>
2017-07-02 13:35:45 -0300

    verifica se professor é coordenador de tcc

--- src/action/Login.php
+++ src/action/Login.php
@@ -9,6 +9,7 @@ $senha = $_POST["password"];
 
       $aluno = mysqli_query($connection, "SELECT * FROM aluno WHERE matricula = '".$usuario."' AND senha = '".$senha."';") or die("erro ao selecionar");
       $professor = mysqli_query($connection, "SELECT * FROM professor WHERE siape = '".$usuario."' AND senha = '".$senha."';") or die("erro ao selecionar");
+      $coordenador = mysqli_query($connection, "SELECT * FROM turma WHERE Professor_siape = '".$usuario."';") or die("erro ao selecionar");
 
     if (mysqli_num_rows($aluno)<=0 && mysqli_num_rows($professor)<=0){
 	    echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>";
@@ -18,10 +19,14 @@ $senha = $_POST["password"];
 		    setcookie("matricula", $usuario);
 		    header('Location: http://localhost/grupo4/src/indexAluno.html');
     	} else if (mysqli_num_rows($professor)>0){
+        if(mysqli_num_rows($coordenador)>0){
+        setcookie("siape", $usuario);
+        header('Location: http://localhost/grupo4/src/indexCoordenador.html');
+        } else {
     		setcookie("siape", $usuario);
 	    	header('Location: http://localhost/grupo4/src/indexProfessor.html');
     	}
 	    
     }
-   
+  }
 ?>
