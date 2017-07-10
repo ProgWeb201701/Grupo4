4e2f03ee6f4ef63f32b270c44d4f85bfce0d5f05
DESKTOP-0E6E8CP\Rodrigo <rodrigo.blizzard@hotmail.com>
2017-07-10 04:32:03 -0300

    fixed Todos os checkbox desmarcados, funcionando.

--- src/action/cadastra_interesse.php
+++ src/action/cadastra_interesse.php
@@ -7,6 +7,11 @@ $connection = $conn->getConnection();
 
 $siape = $_SESSION['usuario'];
 
+$sqlDel ="DELETE FROM `prof_has_interesse` WHERE `siape` = '".$siape."';";
+
+$resultado = mysqli_query($connection, $sqlDel) or die ("Erro ao conectar na tabela " . mysqli_error($connection));
+
+if(isset($_POST['check_box']) == true){
 foreach($_POST['check_box'] as $idInteresse){
 $idInteresse = $idInteresse; 
 
@@ -14,7 +19,9 @@ $sql ="INSERT INTO `prof_has_interesse`(`siape`, `idInteresse`) VALUES ('".$siap
 
 $resultado = mysqli_query($connection, $sql) or die ("Erro ao conectar na tabela " . mysqli_error($connection));
 }
-
 echo"<script language='javascript' type='text/javascript'>alert('Areas de interesse alteradas com sucesso!');window.location.href='../perfil.php';</script>";
+}
+echo"<script language='javascript' type='text/javascript'>alert('Todas as areas de interesse foram removidas!');window.location.href='../perfil.php';</script>";
+
 
 ?>
