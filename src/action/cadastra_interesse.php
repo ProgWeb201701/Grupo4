ffb4060f7fa85f1ed0ccf820d6b26fd2abfac564
DESKTOP-0E6E8CP\Rodrigo <rodrigo.blizzard@hotmail.com>
2017-07-10 03:39:51 -0300

    fixed Cadastrando todos os interesses marcados

--- src/action/cadastra_interesse.php
+++ src/action/cadastra_interesse.php
@@ -9,11 +9,11 @@ $siape = $_SESSION['usuario'];
 
 foreach($_POST['check_box'] as $idInteresse){
 $idInteresse = $idInteresse; 
-}
+
 $sql ="INSERT INTO `prof_has_interesse`(`siape`, `idInteresse`) VALUES ('".$siape."', '".$idInteresse."');";
 
 $resultado = mysqli_query($connection, $sql) or die ("Erro ao conectar na tabela " . mysqli_error($connection));
-
+}
 
 header('Location: http://localhost/grupo4/src/perfil.php');
 
